76392b69ca5096c48308b3b1fae85d8a6b2a36ce
freddwithy <134991353+freddwithy@users.noreply.github.com>
2024-02-26 11:05:36 -0300

    preparando proyecto

---
+++ components/footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="bg-white border-t">
+        <div className="mx-auto py-10">
+            <p className="text-center text-xs text-black">© 2024 Tienda</p>
+        </div>
+    </footer>
+  );
+}
+
+export default Footer;
