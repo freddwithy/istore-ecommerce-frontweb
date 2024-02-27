02a86da1a59d4c988b0149984e7c1e0da2d3b79f
freddwithy <134991353+freddwithy@users.noreply.github.com>
2024-02-27 13:26:50 -0300

    creando modal con hooks

---
+++ components/preview-modal.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import usePreviewModal from "@/hooks/use-preview-modal"
+import Modal from "./ui/modal"
+import Gallery from "./gallery"
+import Info from "./info"
+
+const PreviewModal = () => {
+    const PreviewModal = usePreviewModal()
+    const product = usePreviewModal((state) => state.data)
+
+    if(!product) return null
+
+    return (
+        <div>
+            <Modal
+                open={PreviewModal.isOpen}
+                onClose={PreviewModal.onClose}
+            >
+                <div className="grid w-full grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8">
+                    <Gallery images={product.images}/>
+                </div>
+                <div className="sm:col-span-8 lg:col-span-7">
+                    <Info 
+                        data={product}
+                    />
+                </div>  
+            </Modal>
+        </div>
+    )
+}
+
+export default PreviewModal
